5d7fe8bca0b123041606a1fd5dd436ad2536338a
Alex Chalk <alexchalk17@gmail.com>
2016-12-15 20:31:07 -0500

    Improve readability of jQuery code; update readme file.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a simple sketchpad built using Javascript and jQuery. The goal of the project is to improve my competence with Javascript and jQuery and to demonstrate that I have done so.
+This is a simple sketch pad built using Javascript and the jQuery library. The goal of the project is to improve my competence with Javascript and to demonstrate that I have done so.
--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+/* Renders initial sketch pad. */
+
     $('body').append('<div class="container-outer"></div>');
     $('.container-outer').append('<div class="container-inner"></div>');
     $('.container-inner').append('<table></table>');
@@ -9,31 +12,54 @@ $(document).ready(function() {
         }
     }
     $('.container-outer').append('<button>Reset the Sketch Pad</button>');
+
+/* Controls sketch-pad cell behaviour on mouse hover. */ 
+
     $('td').on('mouseenter', (function() {
 	$(this).css('opacity', '+=0.15');
     }));
+
+/* Provides reset sketch pad functionality. */
+
     $('button').click(function() {
-	var $tableDim = ($('table').width())  
-	var $dim = $tableDim / ($('td').width());
-	var lineLength = 16;
-	do {var lineLength = prompt("You've been using a " + $dim + "x" + $dim + " block grid, but you can adjust this now. Enter a number 'X' below, and the new grid will be 'X' blocks high by 'X' blocks wide. (If you don't enter a whole number between 1-100, this command will just repeat.)");
-	   } while(!(lineLength >= 1 && lineLength <= 100 && lineLength % 1 === 0));
-	while($tableDim % lineLength != 0) {
-	    var sortOutTable = Math.round($tableDim / lineLength);
-	    var $tableDim = (lineLength * sortOutTable);
+
+/*Calculates existing cell density of sketch pad and communicates it to user. 
+Also prompts user to redefine cell density, ensuring they enter a valid value.*/
+
+	var tableDimensions = ($('table').width());  
+	var cellDimensions = ($('td').width());
+	var lineLength = tableDimensions / cellDimensions;
+	do {var userInput = prompt("You've been using a " + lineLength + "x" + lineLength + " block grid, but you can adjust this now. Enter a number 'X' below, and the new grid will be 'X' blocks high by 'X' blocks wide. (If you don't enter a whole number between 1-100, this command will just repeat.)");
+	   } while(!(userInput >= 1 && userInput <= 100 && userInput % 1 === 0));
+	var lineLength = userInput;
+	
+/* Ensures cells fit neatly within table */
+
+	while(tableDimensions % lineLength != 0) {
+	    var sortOutTable = Math.round(tableDimensions / lineLength);
+	    var tableDimensions = (lineLength * sortOutTable);
 	}
-	var tdDim = ($tableDim / lineLength);
+	var cellDimensions = (tableDimensions / lineLength);
+
+/* Empties existing table */
+
 	$('table').empty();
+
+/* Re-renders table in line with new user specifications. */
+
 	for (var i=1; i<=lineLength ; i++) {
 	    $('table').append('<tr></tr>');
 	    for (var j=1; j<=lineLength; j++) {
 		$('table tr:nth-last-child(1)').append('<td></td>');
 	    }
 	}
-	$('table').height($tableDim + "px");
-	$('table').width($tableDim + "px");
-	$('td').height(tdDim + "px");
-	$('td').width(tdDim + "px");
+	$('table').height(tableDimensions + "px");
+	$('table').width(tableDimensions + "px");
+	$('td').height(cellDimensions + "px");
+	$('td').width(cellDimensions + "px");
+
+/* Re-implements sketch-pad cell behaviour on mouse hover. */
+	
 	$('td').on('mouseenter', (function() {
 	    $(this).css('opacity', '+=0.15');
 	}));
